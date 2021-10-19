a959fb640e841c14f0e35e1f77e0317bdd1ecdd6
Cagri Yolyapar <cagri.yolyapar@gmail.com>
2021-10-19 14:02:36 +0300

    Add project files.

---
+++ CoreMVCIntro_1/Models/Context/MyContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMVCIntro_1.Models.Context
+{
+    public class MyContext
+    {
+    }
+}
